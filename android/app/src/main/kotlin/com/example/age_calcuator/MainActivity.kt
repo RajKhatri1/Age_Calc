942e289bd733f89d4cb640c80f1a93f33125e0b4
HP <rajkhatri23066@gmail.com>
2023-01-18 10:38:13 +0530

    Age

---
+++ android/app/src/main/kotlin/com/example/age_calcuator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.age_calcuator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:age_calcuator/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      home: Age(),
+    )
+  );
+}
